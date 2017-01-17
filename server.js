304f43e6b923b9f9b6bfb13ee09f27994612e29d
Sonic Spec <nhimspec@gmail.com>
2017-01-18 01:34:30 +0700

    Update

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function sendMessage(senderId, message) {
   });
 }
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 443);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0');
 
 server.listen(app.get('port'), app.get('ip'), function() {
